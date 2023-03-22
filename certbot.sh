61509e8f5edf86cd9b16d943bbaebc1f2b014283
Mohd Ali <mohd.ali.el@gmail.com>
2023-03-22 16:15:46 -0400

    certbot

---
+++ certbot.sh
@@ -0,0 +1,1 @@
+sudo certbot certonly --manual --preferred-challenges=dns --email mohd.al.el@gmail.com  --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d orbiter-mods.com -d *.orbiter-mods.com
