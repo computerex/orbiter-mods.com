4a75317a06ef5bcf41a75aa67175967eb5e5d920
Mohd Ali <mohd.ali.el@gmail.com>
2023-10-12 22:29:11 -0400

    update

--- web/public/index.php
+++ web/public/index.php
@@ -99,6 +99,33 @@ $app->get('/search', function($request, Response $response) {
     return $response->withJson($results);
 });
 
+$app->get('/chat', function($request, Response $response) {
+
+    // Get the request body from the incoming request
+    $requestBody = $request->getBody();
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'chat:4000/chat',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $requestBody,
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return $response;
+});
 
 $app->get('/', function ($request, Response $response) {
     // serve static index html file
